4f181c4705e925bfb524a70fd7aa5269508cf734
Florin Cosmin <florincosmin@fastmail.com>
2018-08-16 19:22:34 +0200

    started to work on javascript part

---
+++ src/js/main.js
@@ -0,0 +1,39 @@
+/**
+ * 1. global context or this or window
+ */
+// console.log(window);
+
+/**
+ * 2. call a method from window object
+ */
+// var secondsPassed = 1;
+// var intervalId = window.setInterval(function() {
+//   console.log('seconds passed', secondsPassed);
+//   secondsPassed = secondsPassed + 1;
+//   if(secondsPassed === 11) {
+//     window.clearInterval(intervalId);
+//   }
+// }, 1000);
+
+/**
+ * 3. call a method (setTimeout) from window object
+ */
+// window.setTimeout(function(){
+//   console.log('hello from delayed function')
+// }, 3000);
+// console.log('hello fcc app');
+
+// $(document).ready(function() {
+//   console.log('document is ready');
+// });
+
+// jquery on load function -> page has been loaded
+// https://learn.jquery.com/using-jquery-core/document-ready/
+$(function onPageLoad() {
+
+  // here we will put all our code
+
+
+
+
+});
