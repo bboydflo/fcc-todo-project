8fe4537af69f91fe985ede5119f20b546a132220
Florin <florincosmin@fastmail.com>
2018-09-13 19:33:13 +0200

    fixed small bugs

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,11 @@
+// a class has properties and methods
 class TodoApp {
   constructor($rootEl) {
     this.todos = []
+
+    // saving the refference to the root element
     this.$rootEl = $rootEl
+
     this.titleSelector = 'h1'
     this.newTodoForm = 'form'
     this.newTodoInputSelector = 'form input'
@@ -27,29 +31,42 @@ class TodoApp {
   addNewTodo (newTodo) {
     this.todos.push(newTodo)
   }
-}
 
+  getTodos() {
+    return this.todos;
+  }
+}
 
 const onLoad = () => {
   let $todoItem1 = $('[data-item="todo1"]')
   let todoApp = new TodoApp($todoItem1)
 
   let $todoForm = todoApp.$getNewTodoForm()
+
+  // attach an event listener on the $todoForm
   $todoForm.on('submit', (ev) => {
 
     // prevent the default behaviour
     ev.preventDefault()
 
+    let $inputField = todoApp.$getNewTodoInput()
+
     // get the actual input value (this refers to the global object -> window)
-    let $inputValue = todoApp.$getNewTodoInput().val()
+    let $inputValue = $inputField.val()
 
-    // save it into the todo items list
-    todoApp.addNewTodo($inputValue)
+    if ($inputValue.length > 4) {
+      // save it into the todo items list
+      todoApp.addNewTodo($inputValue)
 
-    console.log(todoApp.todos)
+      $inputField.val('')
+    } else {
+      throw new Error('the todo is not long enough')
+    }
+
+    console.log(todoApp.getTodos())
   })
 }
 
 // jquery on load function -> page has been loaded
 // https://learn.jquery.com/using-jquery-core/document-ready/
-$(onLoad);
+$(onLoad)
