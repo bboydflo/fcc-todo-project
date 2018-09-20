068196e18ef5193bb1f2415ddbc01b8974ea63b3
Florin <florincosmin@fastmail.com>
2018-09-20 19:07:57 +0200

    added alert box

--- src/js/main.js
+++ src/js/main.js
@@ -9,6 +9,8 @@ class TodoApp {
     this.titleSelector = 'h1'
     this.newTodoForm = 'form'
     this.newTodoInputSelector = 'form input'
+    this.alertBoxSelector = '.alert'
+    this.closeSelector = '.close'
   }
 
   $getTitle () {
@@ -35,35 +37,59 @@ class TodoApp {
   getTodos() {
     return this.todos;
   }
+
+  $getAlertBox() {
+    return this.$rootEl.find(this.alertBoxSelector)
+  }
+
+  $getCloseButton() {
+    return this.$rootEl.find(this.closeSelector)
+  }
 }
 
 const onLoad = () => {
   let $todoItem1 = $('[data-item="todo1"]')
-  let todoApp = new TodoApp($todoItem1)
+  let todoApp1 = new TodoApp($todoItem1)
 
-  let $todoForm = todoApp.$getNewTodoForm()
+  let $todoForm1 = todoApp1.$getNewTodoForm()
+  let $alertBox1 = todoApp1.$getAlertBox()
+  let $closeButton1 = todoApp1.$getCloseButton()
 
-  // attach an event listener on the $todoForm
-  $todoForm.on('submit', (ev) => {
+  $closeButton1.on('click', () => {
+    $alertBox1.addClass('d-none')
+  })
+
+  // attach an event listener on the $todoForm1
+  $todoForm1.on('submit', (ev) => {
 
     // prevent the default behaviour
     ev.preventDefault()
 
-    let $inputField = todoApp.$getNewTodoInput()
+    let $inputField = todoApp1.$getNewTodoInput()
 
     // get the actual input value (this refers to the global object -> window)
     let $inputValue = $inputField.val()
 
     if ($inputValue.length > 4) {
-      // save it into the todo items list
-      todoApp.addNewTodo($inputValue)
 
+      // save it into the todo items list
+      todoApp1.addNewTodo($inputValue)
+
+      // clear the input field
       $inputField.val('')
+
+      // hide the alert box
+      $alertBox1.removeClass('show').addClass('d-none')
     } else {
-      throw new Error('the todo is not long enough')
+
+      // here we show the alert box
+      $alertBox1.find('.alert-message').html(
+        `<strong>"${$inputValue}"</strong>  is not long enough! (min 4)`
+      )
+      $alertBox1.removeClass('d-none').addClass('show')
     }
 
-    console.log(todoApp.getTodos())
+    console.log(todoApp1.getTodos())
   })
 }
 
